5be99100340b6181037df44f0e1cc680b8b02fb4
mhans <mhanssler@gmail.com>
2023-12-20 22:38:54 -0800

    EA Power Supply Control

---
+++ Power_Supply_Control.py
@@ -0,0 +1,41 @@
+import pyvisa
+
+class EAPSB9000Controller:
+    def __init__(self, resource_name):
+        self.rm = pyvisa.ResourceManager()
+        self.instr = self.rm.open_resource(resource_name)
+
+    def close(self):
+        self.instr.close()
+
+    def send_command(self, command):
+        self.instr.write(command)
+
+    def query(self, command):
+        return self.instr.query(command)
+
+    # Add specific commands for your power supply here
+    def set_voltage(self, voltage):
+        command = f"VOLT {voltage}"
+        self.send_command(command)
+
+    def set_current(self, current):
+        command = f"CURR {current}"
+        self.send_command(command)
+
+    def get_voltage(self):
+        return self.query("MEAS:VOLT?")
+
+    def get_current(self):
+        return self.query("MEAS:CURR?")
+
+    # More specific commands can be added here...
+
+# Example usage
+if __name__ == "__main__":
+    # Replace with your instrument's address
+    psu = EAPSB9000Controller('GPIB0::5::INSTR')
+    psu.set_voltage(5.0)
+    print("Voltage set to 5.0 V")
+    print(f"Current Voltage: {psu.get_voltage()} V")
+    psu.close()
