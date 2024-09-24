766f9713ef5b8ffce40dad6c6f2450d6aeb43aaa
mhanssler <mhanssler@gmail.com>
2024-09-24 15:50:34 -0700

    made changes to the code and added try except block

--- lab_projects/scripts/Rigol_Instrument_Control.py
+++ lab_projects/scripts/Rigol_Instrument_Control.py
@@ -1,9 +1,13 @@
 import pyvisa
+import Power_Supply_Control
 
 def list_devices():
-    rm = pyvisa.ResourceManager()
-    return rm.list_resources()
-
+    try:
+        rm = pyvisa.ResourceManager()
+        return rm.list_resources()
+    except Exception as e:
+        print("Error listing devices:", e)
+        return []
 def ps_set_voltage(instrument, voltage, channel):
     # Ensure voltage is a string. If it's a numeric type, convert it.
     voltage = str(voltage)
@@ -18,14 +22,19 @@ def ps_set_voltage(instrument, voltage, channel):
 
 #def scope_capture_autoset()
 
-
-
 def connect_device(address):
-    rm = pyvisa.ResourceManager()
-    return rm.open_resource(address)
+    try:
+        rm = pyvisa.ResourceManager()
+        return rm.open_resource(address)    
+    except Exception as e:
+        print("Error connecting to device:", e)
+        return None
 
 def main():
     devices = list_devices()
+    if not devices:
+        print("No devices found")
+        return
     print("Connected devices:", devices)
     voltage = input("Please Enter the Voltage for the Power Supply 0-30 ")
     channel = input("please Enter the Channel for the Power Supply 1, 2 ")
@@ -36,10 +45,16 @@ def main():
     #O_scope = 'USB0::0x1AB1::0x0588::DS1ET153818956::INSTR'
 
     instrument = connect_device(power_supply)
-    ps_set_voltage(instrument, voltage, channel)
-    # Replace with actual SCPI commands for your instruments
-    instrument.write('*IDN?')
-    print("",instrument.read())
+    if instrument:
+        ps_set_voltage(instrument, voltage, channel)
+        try:
+            instrument.write('*IDN?')
+            print("Device ID",instrument.read())
+        except Exception as e:
+            print("Error sending command:", e)
+    else:
+        print("Device not connected")
 
 if __name__ == "__main__":
     main()
+
